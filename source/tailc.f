b413e8ea0cc0061b0baff464420a02c3fb0db16e
Michelle Liu <michelle.liu@berkeley.edu>
2015-06-17 11:46:07 -0700

    Changed Pi to 4.0d0*Datan(1.0d0)

--- source/tailc.f
+++ source/tailc.f
@@ -5,6 +5,8 @@
 
 C     Compute The Tail Correction Energy Of A Particle
 
+C     THIS IS ONLY FOR PURE COMPONENT!! Otherwise Must Change Densities
+
       Integer Ib,Mytype
       Double Precision Utail,Rho,R3,S3
 
@@ -17,7 +19,7 @@ C     Compute The Tail Correction Energy Of A Particle
       R3 = S3*1.0d0/R3
       R3 = S3*R3*(R3*R3/3.0d0-1.0d0)
 
-      Utail = (8.0d0/3.0d0)*Pi*Rho*Eps(Mytype,Mytype)*R3
+      Utail = (8.0d0/3.0d0)*4.0d0*Datan(1.0d0)*Rho*Eps(Mytype,Mytype)*R3
 
       Return
       End
